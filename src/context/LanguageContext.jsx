82b5e8fb23a8a4bc7969507149b26fc1e5eec17b
Veregorn <rjbarco@gmail.com>
2024-08-09 07:33:42 +0200

    '@astro/react' package configured in 'astro.config.mjs' to be able to use React components and context in the project

---
+++ src/context/LanguageContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from 'react';
+
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState('spanish');
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext);
+
+  if (!context) {
+    throw new Error('useLanguage must be used within a LanguageProvider');
+  }
+
+  return context;
+};
